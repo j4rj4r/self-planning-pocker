96c9b38c67683be50ca64e88cab6d7d51c5018e3
Jarod CAJNA <jarod.cajna@gatewatcher.com>
2026-06-17 17:03:41 +0200

    Add business value

--- angular/src/app/model/deck.ts
+++ angular/src/app/model/deck.ts
@@ -82,6 +82,22 @@ export const decks: Deck[] = [
       { value: 6, display: 'XL' },
       { value: 7, display: 'XXL' },
     ]
+  },
+  {
+    name: 'BUSINESS_VALUE',
+    textValues: false,
+    values: [
+      { value: 1, display: 1 },
+      { value: 2, display: 2 },
+      { value: 3, display: 3 },
+      { value: 4, display: 4 },
+      { value: 5, display: 5 },
+      { value: 6, display: 6 },
+      { value: 7, display: 7 },
+      { value: 8, display: 8 },
+      { value: 9, display: 9 },
+      { value: 10, display: 10 },
+    ]
   }
 ]
 
